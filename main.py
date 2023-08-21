b4a6ae07ddf6f66f0b1ecb372a1a2031aca87942
Paul <gpu2003@gmail.com>
2023-08-21 18:38:24 +0100

    view all, add routes created

---
+++ main.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+import sqlite3 as sql
+
+# Define a Function to establish a new connection to the database
+def get_db_connection():
+    dbCon = sql.connect("square_share.db") #connection object
+    dbCursor = dbCon.cursor() #cursor object
+    return dbCon, dbCursor
+
+# Ensure the table exists before starting the app
+conn, cursor = get_db_connection()
+
+# Create the flats table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS "flats" (
+"flatID" INTEGER PRIMARY KEY AUTOINCREMENT,
+"fname" TEXT NOT NULL
+)""")
+
+# Create the items table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS "items" (
+"itemID" INTEGER PRIMARY KEY AUTOINCREMENT, 
+"flatID" INTEGER NOT NULL,
+"class" VARCHAR (10),
+"item_name" VARCHAR (50),
+"availability" VARCHAR (20), 
+FOREIGN KEY (flatID) REFERENCES flats (flatID)               
+)""")
+
+# Create the items table
+conn.commit()
+conn.close()
+
+# CRUD:
+# Add Items
+# Delete Items
+# Update Items
+# View All Items
+
+# Home Page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/items")
+def view_items():
+  conn, cursor = get_db_connection()
+  cursor.execute("SELECT * FROM items")
+  items = cursor.fetchall()
+  conn.close()
+  return render_template("items.html", items=items)
+
+# add a new item to the db
+@app.route("/add_items", methods=["GET", "POST"])
+def add_items():
+    if request.method == "POST":
+        flat = request.form["flatID"]
+        fname = request.form["fname"]
+        classn = request.form["class"]
+        item = request.form["item_name"]
+        availability = request.form["availability"]
+        
+        conn, cursor = get_db_connection()
+        
+        # Check if the flat already exists, if not insert
+        cursor.execute("SELECT * FROM flats WHERE flatID = ?", (flat,))
+        existing_flat = cursor.fetchone()
+        if not existing_flat:
+            cursor.execute("INSERT INTO flats (flatID, fname) VALUES (?, ?)", (flat, fname))
+
+        cursor.execute("INSERT INTO items (flatID, class, item_name, availability) VALUES (?, ?, ?, ?)", (flat, classn, item, availability))
+        conn.commit()
+        conn.close()
+        
+        return redirect(url_for('home'))  # Redirect to the main page after adding
+
+    return render_template("add_items.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
