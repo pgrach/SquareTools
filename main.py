6a87fdce61ac72721e7193b5c4ead14699d5209e
Paul <gpu2003@gmail.com>
2023-08-22 10:58:43 +0100

    restructured tables, adding members table and reformulated routes making sure same name is not saved twise in the same flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+
+# created an instance of the Flask application 
 app = Flask(__name__)
 
 import sqlite3 as sql
@@ -9,25 +11,34 @@ def get_db_connection():
     dbCursor = dbCon.cursor() #cursor object
     return dbCon, dbCursor
 
-# Ensure the table exists before starting the app
+# establish a connection and get the cursor object for executing SQL statements
 conn, cursor = get_db_connection()
 
 # Create the flats table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS "flats" (
-"flatID" INTEGER PRIMARY KEY AUTOINCREMENT,
-"fname" TEXT NOT NULL
+"flatID" INTEGER PRIMARY KEY AUTOINCREMENT
 )""")
 
-# Create the items table
+# Create the Members Table that will store the names of members (or tenants) of a flat
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS "members" (
+"flatID" INTEGER NOT NULL,
+"memberID" INTEGER PRIMARY KEY AUTOINCREMENT,
+"fname" TEXT NOT NULL,
+FOREIGN KEY (flatID) REFERENCES flats (flatID),
+UNIQUE(flatID, fname)
+)""")
+
+# Create the items table that will store items associated with a flat
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS "items" (
-"itemID" INTEGER PRIMARY KEY AUTOINCREMENT, 
+"itemID" INTEGER PRIMARY KEY AUTOINCREMENT,
 "flatID" INTEGER NOT NULL,
 "class" VARCHAR (10),
 "item_name" VARCHAR (50),
-"availability" VARCHAR (20), 
-FOREIGN KEY (flatID) REFERENCES flats (flatID)               
+"availability" VARCHAR (20),
+FOREIGN KEY (flatID) REFERENCES flats (flatID)
 )""")
 
 # Create the items table
@@ -40,10 +51,20 @@ conn.close()
 # Update Items
 # View All Items
 
+#Define Flask routes
+
 # Home Page
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/members")
+def view_members():
+  conn, cursor = get_db_connection()
+  cursor.execute("SELECT * FROM members")
+  members = cursor.fetchall()
+  conn.close()
+  return render_template("members.html", members=members)
 
 @app.route("/items")
 def view_items():
@@ -64,14 +85,24 @@ def add_items():
         availability = request.form["availability"]
         
         conn, cursor = get_db_connection()
-        
+
         # Check if the flat already exists, if not insert
         cursor.execute("SELECT * FROM flats WHERE flatID = ?", (flat,))
         existing_flat = cursor.fetchone()
         if not existing_flat:
-            cursor.execute("INSERT INTO flats (flatID, fname) VALUES (?, ?)", (flat, fname))
+            cursor.execute("INSERT INTO flats (flatID) VALUES (?)", (flat,))
 
+        # Check if a member with the same name exists in the same flat
+        cursor.execute("SELECT * FROM members WHERE flatID = ? AND fname = ?", (flat, fname))
+        existing_member = cursor.fetchone()
+
+        # If the member doesn't exist, add them
+        if not existing_member:
+            cursor.execute("INSERT INTO members (flatID, fname) VALUES (?, ?)", (flat, fname))
+
+        # Insert into items
         cursor.execute("INSERT INTO items (flatID, class, item_name, availability) VALUES (?, ?, ?, ?)", (flat, classn, item, availability))
+
         conn.commit()
         conn.close()
         
